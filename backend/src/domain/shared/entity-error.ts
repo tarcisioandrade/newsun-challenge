cea911e7f9499b9fcac7142c43e5d2549357cb60
Tarcisio <tarcisioandrade016@gmail.com>
2023-12-31 09:31:11 -0300

    chore: delete unnosed files

--- backend/src/domain/shared/entity-error.ts
+++
@@ -1,8 +0,0 @@
-export class EntityError extends Error {
-  constructor(message: string) {
-    super(message);
-
-    // Manter a propriedade do protótipo para a cadeia de protótipos funcionar corretamente
-    Object.setPrototypeOf(this, EntityError.prototype);
-  }
-}
--- backend/src/domain/value-objects/id.ts
+++ backend/src/domain/value-objects/id.ts
@@ -1,8 +1,7 @@
 import { v4 as uuidv4, validate } from "uuid";
-import { EntityError } from "../shared/entity-error";
 
 export class Id {
   constructor(readonly value: string = uuidv4()) {
-    if (!validate(value)) throw new EntityError("Id inválido");
+    if (!validate(value)) throw new Error("Id inválido");
   }
 }
